0fe000cf6af5b6a14e6c8c871487a254420f455a
LironSaedi <lionsaedi@gmail.com>
2017-09-13 17:33:00 -0700

    Finished the neo pixels ring going around in one color

--- LironNeoPixelRing.ino
+++ LironNeoPixelRing.ino
@@ -6,30 +6,32 @@ Adafruit_NeoPixel ring (12, 6);
 void setup() {
   // put your setup code here, to run once:
   ring.begin();
-  ring.setBrightness(20);
+  ring.setBrightness(10);
 }
 
 void loop()
 {
   // put your main code here, to run repeatedly:
-  if (i < 11)
+  if (i < 12)
   {
-    delay(delayv);
+    
+    ring.clear();
     ring.setPixelColor(i, ring.Color(0, 255, 185));
-    delay(delayv);
     ring.show();
+    delay(delayv);
+    i++;
+    ring.clear();
 
-    delay(delayv);
-    ring.setPixelColor(i, ring.Color(0, 0, 0));
-    delay(delayv);
-    ring.setPixelColor(i, ring.Color(255, 0, 0));
-    ring.show();
 
-    delay(delayv);
-    ring.setPixelColor(i, ring.Color(0, 0, 0));
-    delay(delayv);
-    ring.setPixelColor(i, ring.Color(0, 0, 255));
-    ring.show();
+  }
+  if (i == 12)
+  {
+    i = 0;
+    ring.clear();
   }
 }
-
+//    delay(delayv);
+//    ring.setPixelColor(i, ring.Color(0, 0, 0));
+//    delay(delayv);
+//    ring.setPixelColor(i, ring.Color(0, 0, 255));
+//    ring.show();
